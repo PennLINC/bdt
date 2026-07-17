30b15eb5e879c5b1a8a46c5c33678128cc5bbe0e
Taylor Salo <tsalo90@gmail.com>
2026-07-17 12:46:43 -0400

    Run local test.

---
+++ scripts/run_nifti_parcellate.sh
@@ -0,0 +1,9 @@
+bdt \
+    /mnt/c/Users/tsalo/Documents/datasets/ds008325 \
+    /mnt/c/Users/tsalo/Documents/datasets/ds008325-derivatives/bdt-nifti-parcellate \
+    participant \
+    --spec nifti_parcellate.yml \
+    --participant-label 125511 \
+    --datasets \
+    fmriprep=/mnt/c/Users/tsalo/Documents/datasets/ds008325/derivatives/fmriprep_func \
+    atlases=/mnt/c/Users/tsalo/Documents/datasets/AtlasPack
